cac7a6b4f3734212d3acce1c3c46ebf01fbc8e58
Gagan Kaul <63837949+gagankaul@users.noreply.github.com>
2022-04-05 16:42:20 -0400

    updated

--- binarytohex.py
+++ binarytohex.py
@@ -1,13 +1,10 @@
-
+# A program to convert integers into binary and hexadecimal numbers
 
 print("Welcome to the Binary/Hexadecimal Converter App\n")
 
-num = int(input("Compute binary and hexadecimal values up to the following decimal number: "))
-print("Generating lists .... complete!\n")
+num = int(input("Compute binary and hexadecimal values up to the following number: "))
 
-print("Using slices, we will now show a portion of each list.")
-start_num = int(input("What decimal number would you like to start at: "))
-end_num = int(input("What decimal number would you like to stop at: "))
+# Generating three lists - integers, binary numbers, hexadecimal numbers
 
 dec_list = []
 bin_list = []
@@ -16,11 +13,14 @@ hex_list = []
 for i in range(1,num+1):
     dec_list.append(i)
     bin_list.append(bin(i))
-
-    # bin_num = "{0.b}".format(i)
-    # bin_list.append(bin_num)
-
     hex_list.append(hex(i))
+
+# Getting user input for list slicing
+
+print("Generating lists .... complete!\n")
+print("Using slices, we will now show a portion of each list.")
+start_num = int(input("What number would you like to start at: "))
+end_num = int(input("What number would you like to stop at: "))
 
 print("\nDecimal Values from " + str(start_num) + " to " + str(end_num))
 for i in range(start_num-1, end_num):
@@ -34,9 +34,10 @@ print("\nHexadecimal Values from " + str(start_num) + " to " + str(end_num))
 for i in range(start_num-1, end_num):
     print(hex_list[i])
 
+# Printing the complete list
+
 input("Press Enter to see all values from 1 to 40.")
 print("Decimal----Binary----Hexadecimal")
 print("----------------------------------------------------")
-
 for dec_num, bin_num, hex_num in zip(dec_list, bin_list, hex_list):
     print(str(dec_num) + "----" + str(bin_num) + "----" + str(hex_num))
